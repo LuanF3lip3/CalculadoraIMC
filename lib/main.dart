e7795a530d6a49fa756096bff9e7c5fc66d466eb
Luan Felipe <luan_felipe2912@hotmail.com>
2022-04-20 22:39:39 -0400

    termino da aula 122 e do app calculadora de IMC / 20.04.22

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,49 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+TextEditingController weightController = TextEditingController();
+TextEditingController heightController = TextEditingController();
+
+GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+String _infoText = "Informe seus dados!!!";
+
+void _resetFilds (){
+
+  weightController.text = "";
+  heightController.text = "";
+  setState(() {
+  _infoText = "Informe os dados!!!";
+  });
+
+  _formKey = GlobalKey<FormState>();
+}
+
+void _calculate(){
+
+  double weight = double.parse(weightController.text);
+  double height = double.parse(heightController.text) / 100;
+  double imc = weight / (height * height);
+setState(() {
+if(imc < 18.6){
+  _infoText = "Abaixo do peso ${imc.toStringAsPrecision(3)}";
+}else if(imc >= 18.6 && imc < 24.8){
+  _infoText = "Peso ideal ${imc.toStringAsPrecision(3)}";
+}else if(imc >= 24.9 && imc < 29.8){
+  _infoText = "Acima do peso ${imc.toStringAsPrecision(3)}";
+}else if(imc >= 29.9 && imc < 34.8){
+  _infoText = "Obesidade gral I ${imc.toStringAsPrecision(3)}";
+}else if(imc >= 34.9 && imc < 40){
+  _infoText = "Obesidade Gral II ${imc.toStringAsPrecision(3)}";
+}else if(imc >= 40){
+  _infoText = "Obesidade Gral III ${imc.toStringAsPrecision(3)}";
+}   
+});
+weightController.clear();
+heightController.clear();
+}
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
@@ -24,48 +67,70 @@ class _HomeState extends State<Home> {
         centerTitle: true,
         backgroundColor: Colors.black,
         actions: [
-          IconButton(onPressed: (){},
+          IconButton(onPressed: ()=> _resetFilds(),
           icon: const Icon(Icons.person_add))
         ],
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-        child: Column(
-          children: [
-            const Icon(Icons.person_outlined,
-            size: 200,),
-            const Text("Coloque suas informaçoes!!!",style: TextStyle(fontSize: 22,
-            color: Colors.red),),
-             const TextField(
-               keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                // label: Text('Peso (Kg)'),
-                labelText: 'Peso (kg)',
-                labelStyle: TextStyle(color:Colors.black)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black, fontSize: 24.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              const Icon(Icons.person_outlined,
+              size: 200,),
+              Text(_infoText,
+              style: const TextStyle(fontSize: 22,
+              color: Colors.red),),
+                TextFormField(
+                 keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  // label: Text('Peso (Kg)'),
+                  labelText: 'Peso (kg)',
+                  labelStyle: TextStyle(color:Colors.black)),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.black, fontSize: 24.0),
+                  controller: weightController,
+                  validator: (value) {
+                    if(value!.isEmpty){
+                      return "Insira um peso!";
+                    }
+                    return null;
+                  },
+                ),
+               TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  // label: Text('Peso (Kg)'),
+                  labelText: 'Altura (cm)',
+                  labelStyle: TextStyle(color:Colors.black)),
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.black, fontSize: 24.0),
+                  controller: heightController,
+                   validator: (value) {
+                    if(value!.isEmpty){
+                      return "Insira uma altura!";
+                    }
+                    return null;
+                  },
               ),
-            const TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                // label: Text('Peso (Kg)'),
-                labelText: 'Altura (cm)',
-                labelStyle: TextStyle(color:Colors.black)),
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.black, fontSize: 24.0),
-            ),
-             Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              child: SizedBox(
-                height: 50,
-                width: double.infinity,
-                child: ElevatedButton(onPressed: (){},
-                child: const Text('Calcular', style: TextStyle(fontSize: 20),),
-                style: ElevatedButton.styleFrom(primary: Colors.black,),
+               Padding(
+                padding: const EdgeInsets.only(top: 10, bottom: 10),
+                child: SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: ElevatedButton(onPressed: (){
+                    if(_formKey.currentState!.validate()){
+                      _calculate();
+                    }
+                  },
+                  child: const Text('Calcular', style: TextStyle(fontSize: 20),),
+                  style: ElevatedButton.styleFrom(primary: Colors.black,),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
